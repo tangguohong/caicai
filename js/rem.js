77350177320460093268dc8fa87d9fb3857789a8
tangguohong <28395347952qq.com>
2016-10-08 22:28:40 +0800

    唐

---
+++ js/rem.js
@@ -0,0 +1,9 @@
+
+			window.onload=window.onresize=function(){
+				var rem = document.documentElement.clientWidth*20/320;
+				document.documentElement.style.fontSize = rem+'px';
+			};
+			
+
+			
+			
